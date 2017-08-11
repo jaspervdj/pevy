e2623aab1f54a3b24883bf380fd64424ee96782c
Jasper Van der Jeugt <m@jaspervdj.be>
2017-08-11 21:12:14 +0200

    Initial commit

--- pevy/printer.py
+++ pevy/printer.py
@@ -1,6 +1,24 @@
+import subprocess
+
 class Printer:
     def __init__(self, logger):
         self.logger = logger
 
     def print(self, item):
-        print('>>> ' + item.text)
+        proc_input = '>>> ' + item.author + ' >>> ' + item.text
+        proc_input = proc_input.encode('utf-8')
+
+        command = ['cat', '-']
+        proc = subprocess.Popen(command,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+
+        (outs, errs) = proc.communicate(proc_input)
+        exit_code = proc.returncode
+        if exit_code == 0:
+            self.logger.info('lpr process finished normally')
+        else:
+            self.logger.error(
+                    'lpr process exited with code {}'.format(str(exit_code)))
+            self.logger.error(errs)
